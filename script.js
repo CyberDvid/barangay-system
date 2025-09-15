091ffa70a9e88b45357b7dfafa03fcbfdd574a7b
devssss <jona.casinillo.coc@phinmaed.com>
2025-09-15 20:15:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scroll for nav links
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            const target = document.querySelector(href);
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 60, // offset for navbar
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
